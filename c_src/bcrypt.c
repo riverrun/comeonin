e564ba6be7d6bc28cb7187b1e88e3c757f008f40
jazzyb <json.barnes@gmail.com>
2015-04-13 17:56:31 -0400

    bcrypt_expandstate yields execution back to erlang

--- c_src/bcrypt.c
+++ c_src/bcrypt.c
@@ -34,15 +34,13 @@
  * algorithm was implemented by Jason M Barnes.
  */
 
-// TODO remove when done debugging
-#include <stdio.h>
-
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <string.h>
+#include <sys/time.h>
 #include <pwd.h>
 
 #include "erl_nif.h"
@@ -66,6 +64,8 @@ static int decode_base64(u_int8_t *, size_t, const char *);
 static ERL_NIF_TERM bcrypt_expandstate(ErlNifEnv *, int, const ERL_NIF_TERM *);
 static ERL_NIF_TERM bcrypt_fini(ErlNifEnv *, int, const ERL_NIF_TERM *);
 static void secure_bzero(void *, size_t);
+static int calc_percent(int *, struct timeval *, struct timeval *);
+static unsigned long adjust_max_per_slice(int, int, u_int32_t, unsigned long);
 
 static const u_int8_t Base64Code[] =
 "./ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -202,20 +202,17 @@ bcrypt(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 	char key[1024];
 	char saltbuf[1024];
 	const char *salt = saltbuf;
-	ERL_NIF_TERM newargv[7];
+	ERL_NIF_TERM newargv[9];
 
-	if (argc != 2) {
+	if (argc != 2)
 		goto inval;
-	}
 
 	/* Get our password and salt from argv */
-	if (!enif_get_string(env, argv[0], key, sizeof(key), ERL_NIF_LATIN1)) {
+	if (!enif_get_string(env, argv[0], key, sizeof(key), ERL_NIF_LATIN1))
 		goto inval;
-	}
 	if (!enif_get_string(env, argv[1], saltbuf, sizeof(saltbuf),
-				ERL_NIF_LATIN1)) {
+				ERL_NIF_LATIN1))
 		goto inval;
-	}
 
 	/* Check and discard "$" identifier */
 	if (salt[0] != '$')
@@ -285,8 +282,10 @@ bcrypt(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 	newargv[4] = enif_make_ulong(env, (unsigned long)rounds);
 	newargv[5] = enif_make_ulong(env, (unsigned long)minor);
 	newargv[6] = enif_make_ulong(env, (unsigned long)logr);
+	newargv[7] = enif_make_ulong(env, 25); /* max_per_slice */
+	newargv[8] = enif_make_ulong(env, 0); /* start_index */
 	return enif_schedule_nif(env, "bcrypt_expandstate", 0,
-			bcrypt_expandstate, 7, newargv);
+			bcrypt_expandstate, 9, newargv);
 
 inval_release_csalt:
 	enif_release_binary(&csalt);
@@ -295,7 +294,22 @@ inval:
 }
 
 /*
- * expand the Blowfish key state
+ * The original bcrypt for-loop that expands the key state resembled the
+ * following:
+ *
+ *     for (k = 0; k < rounds; k++) {
+ *         Blowfish_expand0state(&state, (u_int8_t *) key, key_len);
+ *         Blowfish_expand0state(&state, csalt, salt_len);
+ *     }
+ *
+ * This simple loop has been expanded into the below routine to minimize the
+ * amount of time we stay in C.  In order to meet the definition of a "quickly
+ * running NIF", we should spend no more than 1 millisecond at a time in this
+ * function before relinquishing control back to Erlang.
+ *
+ * The initial invocation of bcrypt_expandstate will attempt 25 rounds of the
+ * blowfish loop and will then try to adjust that value so that subsequent
+ * calls will expend as close to 1 millisecond as possible without going over.
  */
 static ERL_NIF_TERM
 bcrypt_expandstate(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
@@ -306,10 +320,13 @@ bcrypt_expandstate(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 	char key[1024];
 	size_t key_len;
 	u_int32_t rounds;
-	ERL_NIF_TERM newargv[4];
+	int pct, total = 0;
+	struct timeval start, stop;
+	unsigned long max_per_slice, start_index, end;
+	ERL_NIF_TERM newargv[9];
 
 	/* Initialize our data from argv */
-	if (argc != 7 || !enif_inspect_binary(env, argv[0], &state))
+	if (argc != 9 || !enif_inspect_binary(env, argv[0], &state))
 		return enif_make_badarg(env);
 	if (!enif_inspect_binary(env, argv[1], &csalt)) {
 		enif_release_binary(&state);
@@ -319,7 +336,9 @@ bcrypt_expandstate(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 	    !enif_get_ulong(env, argv[3], &key_len_arg) ||
 	    !enif_get_ulong(env, argv[4], &rounds_arg) ||
 	    !enif_get_ulong(env, argv[5], &minor_arg) ||
-	    !enif_get_ulong(env, argv[6], &logr_arg)) {
+	    !enif_get_ulong(env, argv[6], &logr_arg) ||
+	    !enif_get_ulong(env, argv[7], &max_per_slice) ||
+	    !enif_get_ulong(env, argv[8], &start_index)) {
 		enif_release_binary(&state);
 		enif_release_binary(&csalt);
 		return enif_make_badarg(env);
@@ -327,12 +346,48 @@ bcrypt_expandstate(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 	key_len = (size_t)key_len_arg;
 	rounds = (u_int32_t)rounds_arg;
 
+	/* Initialize the number of rounds this slice will try to execute */
+	end = start_index + max_per_slice;
+	if (end > rounds)
+		end = rounds;
+	k = start_index;
+
 	/* Expand state */
-	for (k = 0; k < rounds; k++) {
-		Blowfish_expand0state((blf_ctx *) state.data, (u_int8_t *) key,
-				key_len);
-		Blowfish_expand0state((blf_ctx *) state.data,
-				(u_int8_t *) csalt.data, csalt.size);
+	while (1) {
+		/* Measure how long this slice runs */
+		gettimeofday(&start, NULL);
+		do {
+			Blowfish_expand0state((blf_ctx *) state.data,
+					(u_int8_t *) key, key_len);
+			Blowfish_expand0state((blf_ctx *) state.data,
+					(u_int8_t *) csalt.data, csalt.size);
+		} while (++k < end);
+		if (k == rounds)
+			break;
+		gettimeofday(&stop, NULL);
+
+		pct = calc_percent(&total, &stop, &start);
+		if (enif_consume_timeslice(env, pct)) {
+			/* Scheudle the next slice */
+			max_per_slice = adjust_max_per_slice(max_per_slice,
+					total, k, start_index);
+
+			newargv[0] = enif_make_binary(env, &state);
+			newargv[1] = enif_make_binary(env, &csalt);
+			newargv[2] = argv[2]; /* key variable */
+			newargv[3] = argv[3]; /* key_len variable */
+			newargv[4] = argv[4]; /* rounds variable */
+			newargv[5] = argv[5]; /* minor variable */
+			newargv[6] = argv[6]; /* logr variable */
+			newargv[7] = enif_make_ulong(env, max_per_slice);
+			newargv[8] = enif_make_ulong(env, k);
+			return enif_schedule_nif(env, "bcrypt_expandstate", 0,
+					bcrypt_expandstate, 9, newargv);
+		}
+
+		end += max_per_slice;
+		if (end > rounds)
+			end = rounds;
 	}
 
 	/* Schedule finalization routine */
@@ -431,4 +486,44 @@ secure_bzero(void *buf, size_t len)
 	while (len--) {
 		*ptr++ = 0;
 	}
+}
+
+/*
+ * return the percentage of our timeslice (1 millisecond) that has occurred
+ * between start and stop
+ * called from bcrypt_expandstate()
+ */
+static int
+calc_percent(int *total, struct timeval *start, struct timeval *stop)
+{
+	int pct;
+	struct timeval slice;
+
+	timersub(stop, start, &slice);
+	pct = (int)((slice.tv_sec * 1000000 + slice.tv_usec) / 10);
+	*total += pct;
+
+	if (pct > 100)
+		return 100;
+	else if (pct == 0)
+		return 1;
+	else
+		return pct;
+}
+
+/*
+ * take the current max_per_slice, total, index, and start_index and return
+ * the value of the new max_per_slice given how much we were able to calculate
+ * this slice
+ */
+static unsigned long
+adjust_max_per_slice(int max_per_slice, int total, u_int32_t k,
+		unsigned long start_index)
+{
+	int m = total / 100;
+
+	if (m == 1)
+		return max_per_slice - (max_per_slice * (total - 100) / 100);
+	else
+		return max_per_slice / m;
 }
