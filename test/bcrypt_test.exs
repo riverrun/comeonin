1cf55aafe1d4e88da13c90225de1547ff709db2b
David Whitlock <alovedalongthe@gmail.com>
2015-04-22 17:54:49 +0700

    Updated tests

--- test/bcrypt_test.exs
+++ test/bcrypt_test.exs
@@ -84,10 +84,12 @@ defmodule Comeonin.BcryptTest do
 
   test "hashing and checking passwords" do
     hash_check_password("password", "passwor", "passwords", "pasword")
+    hash_check_password("hard2guess", "ha rd2guess", "had2guess", "hardtoguess")
   end
 
   test "hashing and checking passwords with characters from the extended ascii set" do
-    hash_check_password("password", "passwor", "passwords", "pasword")
+    hash_check_password("aáåäeéêëoôö", "aáåäeéêëoö", "aáåeéêëoôö", "aáå äeéêëoôö")
+    hash_check_password("aáåä eéêëoôö", "aáåä eéê ëoö", "a áåeé êëoôö", "aáå äeéêëoôö")
   end
 
   test "hashing and checking passwords with non-ascii characters" do
