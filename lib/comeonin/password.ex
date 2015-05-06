540444644ad17436174cb7b97ad481db6101aa8f
David Whitlock <alovedalongthe@gmail.com>
2015-05-06 14:14:27 +0700

    Password validator for digits and symbols

---
+++ lib/comeonin/password.ex
@@ -0,0 +1,37 @@
+defmodule Comeonin.Password do
+  @moduledoc """
+  Module to generate random passwords and validate passwords
+  for extra characters.
+  """
+
+  @alphabet './!@#$%^*();:ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  @digits ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  @punc [".", "/", "!", "@", "#", "$", "%", "^", "*", "(", ")", ";", ":"]
+
+  @char_map Enum.map_reduce(@alphabet, 0, fn x, acc -> {{acc, x}, acc + 1} end)
+  |> elem(0) |> Enum.into(%{})
+
+  @doc """
+  """
+  def gen_password(len \\ 8) do
+    case rand_numbers(len) |> pass_check do
+      false -> gen_password(len)
+      code -> for val <- code, do: Map.get(@char_map, val)
+    end
+  end
+  defp rand_numbers(len) do
+    for _ <- 1..len, do: :crypto.rand_uniform(0, 75)
+  end
+  defp pass_check(code) do
+    Enum.any?(code, &(&1 < 13)) and Enum.any?(code, &(&1 > 64)) and code
+  end
+
+  @doc """
+  """
+  def valid_password?(password) do
+    has_punc_digit?(password, @digits, @punc) and password
+  end
+  defp has_punc_digit?(word, digits, punc) do
+    :binary.match(word, digits) != :nomatch and :binary.match(word, punc) != :nomatch
+  end
+end
---
+++ test/password_test.exs
@@ -0,0 +1,34 @@
+defmodule Comeonin.ToolsTest do
+  use ExUnit.Case, async: true
+
+  alias Comeonin.Password
+
+  test "valid password has a digit and a symbol" do
+    for id <- ["hfjkshf6hj#", "8auyk$kjkjh", "ty3uhi@ksd"] do
+      assert Password.valid_password?(id)
+    end
+  end
+
+  test "invalid password has no digit or symbol" do
+    for id <- ["hfjkshfhj", "auykkjkjh", "tyuhiksd"] do
+      refute Password.valid_password?(id)
+    end
+  end
+
+  test "invalid password has no digit" do
+    for id <- ["hf:jksh#fhj", "au$ykkjkjh", "(tyu)hiksd"] do
+      refute Password.valid_password?(id)
+    end
+  end
+
+  test "invalid password has no symbol" do
+    for id <- ["h8fjkshfhj", "auykk2jkj1h", "0tyuhi67ksd"] do
+      refute Password.valid_password?(id)
+    end
+  end
+
+  test "generate valid password" do
+    assert Password.gen_password |> to_string |> Password.valid_password?
+  end
+
+end
