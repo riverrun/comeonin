3ca1aff21331c4506b923720dc7f7fd8a930b652
David Whitlock <alovedalongthe@gmail.com>
2015-05-31 14:22:20 +0700

    Updated documentation

--- lib/comeonin/config.ex
+++ lib/comeonin/config.ex
@@ -12,19 +12,13 @@ defmodule Comeonin.Config do
   | pass_min_length    | integer | 8       |
 
   `crypto_mod` is only needed for some convenience functions in
-  the main Comeonin module. With this setting, you can decide which
-  algorithm you want to use, :bcrypt or :pbkdf2, to hash the password.
+  the main Comeonin module. In many cases, you will not need this.
 
   `bcrypt_log_rounds` and `pbkdf2_rounds` can be used to adjust the
   complexity, and time taken, of the password hashing calculation.
   Please read the documentation for the main Comeonin module,
   which explains why the default values are not always the best
   values to use.
-
-  `pass_length` is for use with the `gen_password` function in the
-  Comeonin.Password module and is the default length of the generated
-  password. `pass_min_length` is for use with the `valid_password?`
-  function in the same module and is the minimum allowed length.
 
   ## Examples
 
@@ -64,30 +58,50 @@ defmodule Comeonin.Config do
   to the `config.exs` file.
   """
 
-  def crypto_mod do
-    Application.get_env(:comeonin, :crypto_mod, :bcrypt)
-  end
-
-  def bcrypt_log_rounds do
-    Application.get_env(:comeonin, :bcrypt_log_rounds, 12)
-  end
-
-  def pbkdf2_rounds do
-    Application.get_env(:comeonin, :pbkdf2_rounds, 60_000)
-  end
-
-  def pass_length do
-    Application.get_env(:comeonin, :pass_length, 12)
-  end
-
-  def pass_min_length do
-    Application.get_env(:comeonin, :pass_min_length, 8)
-  end
-
+  @doc """
+  This value is only used by the `create_hash` and `create_user` functions
+  in the main Comeonin module. You can choose between using bcrypt or
+  pbkdf2_sha512 to hash the password.
+  """
   def get_crypto_mod do
     case crypto_mod do
       :pbkdf2 -> Comeonin.Pbkdf2
       _ -> Comeonin.Bcrypt
     end
   end
+  defp crypto_mod do
+    Application.get_env(:comeonin, :crypto_mod, :bcrypt)
+  end
+
+  @doc """
+  The number of log rounds the bcrypt function uses. The default
+  value of 12 means that 2^12 rounds are used.
+  """
+  def bcrypt_log_rounds do
+    Application.get_env(:comeonin, :bcrypt_log_rounds, 12)
+  end
+
+  @doc """
+  The number of rounds the pbkdf2_sha512 function uses.
+  """
+  def pbkdf2_rounds do
+    Application.get_env(:comeonin, :pbkdf2_rounds, 60_000)
+  end
+
+  @doc """
+  For use with the `gen_password` function, the default length of
+  a password.
+  """
+  def pass_length do
+    Application.get_env(:comeonin, :pass_length, 12)
+  end
+
+  @doc """
+  For use with the `valid_password?` function, the minimum length of
+  a password.
+  """
+  def pass_min_length do
+    Application.get_env(:comeonin, :pass_min_length, 8)
+  end
+
 end
