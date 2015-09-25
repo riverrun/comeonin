bf1e806f02f8c4dbb06b107136a10b3cbec03bc0
David Whitlock <alovedalongthe@gmail.com>
2015-09-25 12:37:20 +0700

    Removed random_bytes function -- calling strong_rand_bytes directly

--- lib/comeonin/tools.ex
+++ lib/comeonin/tools.ex
@@ -5,35 +5,6 @@ defmodule Comeonin.Tools do
 
   use Bitwise
 
-  @alpha Enum.concat ?A..?Z, ?a..?z
-  @alphabet '!#$%&\'()*+,-./:;<=>?@[\\]^_{|}~"' ++ @alpha ++ '0123456789'
-  @char_map Enum.map_reduce(@alphabet, 0, fn x, acc ->
-    {{acc, x}, acc + 1} end)
-    |> elem(0) |> Enum.into(%{})
-
-  @doc """
-  Randomly generate a password.
-
-  The default length of the password is 12 characters, and it is guaranteed
-  to contain at least one digit and one punctuation character.
-  """
-  def gen_password(len \\ 12) do
-    rand_password(len) |> to_string
-  end
-
-  defp rand_password(len) do
-    case rand_numbers(len) |> pass_check do
-      false -> rand_password(len)
-      code -> for val <- code, do: Map.get(@char_map, val)
-    end
-  end
-  defp rand_numbers(len) do
-    for _ <- 1..len, do: :crypto.rand_uniform(0, 80)
-  end
-  defp pass_check(code) do
-    Enum.any?(code, &(&1 < 18)) and Enum.any?(code, &(&1 > 69)) and code
-  end
-
   @doc """
   Compares the two binaries in constant time to avoid timing attacks.
   """
--- test/tools_test.exs
+++ test/tools_test.exs
@@ -1,11 +1,4 @@
 defmodule Comeonin.ToolsTest do
   use ExUnit.Case, async: true
 
-  alias Comeonin.Tools
-
-  test "password default length config" do
-    assert Tools.gen_password(8) |> String.length == 8
-    assert Tools.gen_password(16) |> String.length == 16
-  end
-
 end
